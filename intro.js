88102e9af402457c8608069f6788242abaf4c5b7
Arcade <159921660+Arrcadee@users.noreply.github.com>
2026-05-13 10:26:15 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Chidubem";
+const age = 23;
+const favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
